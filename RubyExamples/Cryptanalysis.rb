c1e06bf3611bd7c0703b35d1623633ec40efaa9e
paul_g <paul_g@LAPTOP-5KT0R36B>
2018-02-06 18:56:36 +1100

    Updated Array excercises Improved encryption code

---
+++ RubyExamples/Cryptanalysis.rb
@@ -0,0 +1,4 @@
+# Count the frequency of each letter
+
+#TODO - create a hash to count the letter frequency
+
--- RubyExamples/Cryptology.rb
+++ RubyExamples/Cryptology.rb
@@ -1,73 +1,90 @@
 puts 'Cryptology tests...'
 
-plaintext_alphabet = Array('A'..'Z')
+# Builds a new cipher alphabet using the Caesar shift
+def build_shiftcipher_alphabet(plaintext_alphabet, shift_count)
+  puts "Building shift cipher alphabet..."
 
-puts "Plaintext alphabet:"
-print plaintext_alphabet ; puts ; puts
-
-encryption_alphabet = Array('A'..'Z')
-
-# Modify the encryption alphabet
-
-=begin
-# Move a letter to the end by name
-deletedLetter = encryption_alphabet.delete("A")
-encryption_alphabet << deletedLetter
-
-# Move a letter to the end by position
-deletedLetter = encryption_alphabet.delete_at(0)
-encryption_alphabet << deletedLetter
-
-# Shift the first letter to the end
-deletedLetter = encryption_alphabet.shift
-encryption_alphabet << deletedLetter
-
-puts "Encryption alphabet:"
-print encryption_alphabet ; puts ; puts
-=end
-
-# Convert keyphrase to unique array of uppercase letters
-keyphrase = "fire in the sky"
-puts keyphrase
-
-keyphrase = keyphrase.upcase
-puts keyphrase
-
-# Extract each letter and put it in an array
-keyphrase_array = Array.new # Start with an empty array
-keyphrase.each_char do |currLetter|
-  if plaintext_alphabet.find_index(currLetter) and keyphrase_array.find_index(currLetter) == nil
-    keyphrase_array << currLetter
+  # Copy the plaintext alphabet to a new cipher alphabet
+  cipher_alphabet = Array.new
+  plaintext_alphabet.each do |current_letter|
+    cipher_alphabet << current_letter
   end
+
+  # Shift the encryption alphabet
+  shift_count.times do
+    cipher_alphabet << cipher_alphabet.shift
+  end
+
+  cipher_alphabet
 end
 
-# Move the keyphrase letters to the start of the encryption alphabet
-keyphrase_array.reverse.each do |current_letter|
-  encryption_alphabet.delete(current_letter)
-  encryption_alphabet.insert(0, current_letter)
-end
+# Builds a new cipher alphabet using the keyphrase
+def build_keyphrase_cipher_alphabet(plaintext_alphabet, keyphrase) 
+  puts "Building keyphrase cipher alphabet..."
 
-puts "Encryption alphabet:"
-print encryption_alphabet ; puts ; puts
-
-plaintext = "Amelie Tabor"
-#puts "Please enter plaintext:"
-#plaintext = gets
-puts plaintext
-plaintext = plaintext.upcase
-
-cyphertext = ""
-
-plaintext.split.each do |current_word| # split plaintext into words
-  current_word.each_char do |current_character|
-    index = plaintext_alphabet.index(current_character) # Lookup letter in plaintext to get index
-    if index != nil
-      cypher_character = encryption_alphabet[index] # Lookup index in the encryption alphabet
-      cyphertext << cypher_character
+  # Copy the plaintext alphabet to a new cipher alphabet
+  cipher_alphabet = Array.new
+  plaintext_alphabet.each do |current_letter|
+    cipher_alphabet << current_letter
+  end
+  
+  keyphrase_array = Array.new # Start with an empty array
+  keyphrase.upcase.each_char do |currLetter|
+    if plaintext_alphabet.find_index(currLetter) and keyphrase_array.find_index(currLetter) == nil
+      keyphrase_array << currLetter
     end
   end
-  cyphertext << " "
+  
+  # Move the keyphrase letters to the start of the encryption alphabet
+  keyphrase_array.reverse.each do |current_letter|
+    cipher_alphabet.delete(current_letter)
+    cipher_alphabet.insert(0, current_letter)
+  end
+  
+  cipher_alphabet
 end
 
-puts cyphertext
+def encrypt(plaintext_alphabet, cipher_alphabet, plaintext)
+  ciphertext = ""
+
+  plaintext.upcase.split.each do |current_word| # split plaintext into words
+    current_word.each_char do |current_character|
+      index = plaintext_alphabet.index(current_character) # Lookup letter in plaintext to get index
+      if index != nil
+        ciphertext << cipher_alphabet[index]
+      end
+    end
+    ciphertext << " " # add a space between words
+  end
+
+  ciphertext
+end
+
+
+plaintext = "Amleie Tabor"
+
+plaintext_alphabet = Array('A'..'Z')
+
+# Use a Caesar shift alphabet
+shift6_cipher_alphabet = build_shiftcipher_alphabet(plaintext_alphabet, 6)
+print shift6_cipher_alphabet; puts; puts
+
+ciphertext = encrypt(plaintext_alphabet, shift6_cipher_alphabet, plaintext)
+puts ciphertext
+
+# Reverse the encryption
+decoded_text = encrypt(shift6_cipher_alphabet, plaintext_alphabet, ciphertext)
+puts decoded_text
+
+puts; puts
+
+# Now use a keyphrase alphabet 
+keyphrase1_cipher_alphabet = build_keyphrase_cipher_alphabet(plaintext_alphabet, "fire in the sky")
+print keyphrase1_cipher_alphabet; puts; puts
+ciphertext = encrypt(plaintext_alphabet, keyphrase1_cipher_alphabet, plaintext)
+puts ciphertext
+
+# Reverse the encryption
+decoded_text = encrypt(keyphrase1_cipher_alphabet, plaintext_alphabet, ciphertext)
+puts decoded_text
 
